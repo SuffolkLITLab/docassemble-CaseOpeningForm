8914fa4d578cdbf22209a4d6f573ae1ff43438d9
Aubrie Souza <66577647+stevejaubz@users.noreply.github.com>
2021-11-17 19:00:15 -0500

    resolve student attorney ask and improve fields to PDF

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.CaseOpeningForm',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.4.1'],
+      install_requires=['docassemble.AssemblyLine>=2.6.2'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/CaseOpeningForm/', package='docassemble.CaseOpeningForm'),
      )
